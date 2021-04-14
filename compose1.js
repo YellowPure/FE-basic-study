00f1f9cf7ba841fc64c59b39cf4cbcdbc1ac4e7b
黄亮 <h.love.pure@gmail.com>
2021-04-14 18:37:10 +0800

    feat: use ramda

--- compose1.js
+++ compose1.js
@@ -1,20 +1,42 @@
 const _ = require('lodash/fp');
-var R = require('ramda')
-// var compose = function (f, g) {
-//   return function (x) {
-//     return f(g(x));
-//   };
-// };
+var R = require('ramda');
 
-// var snakeCase = function (word) {
-//   return word.toLowerCase().replace(/\s+/ig, '_');
-// };
-// snakeCase= R.compose(R.replace(/\s+/ig, '_'), R.toLowerCase);
-// console.log('snakeCase',snakeCase('name'))
+/**
+ * 删除对象中空元素的key
+ */
+let deleteUndefinedValue = (obj) => {
+  const _obj = { ...obj };
+  for (const key of Object.keys({ _obj })) {
+    if (_obj[key] === undefined || _obj[key] === null) {
+      delete _obj[key];
+    }
+  }
+  return _obj;
+};
+deleteUndefinedValue = R.filter(R.compose(R.not, R.isNil));
+const a = { name: 'ssss', bill: null };
+console.log(deleteUndefinedValue(a));
 
-// var initials = function (name) {
-//   return name.split(' ').map(compose(toUpperCase, head)).join('. ');
-// };
+// TODO transfer => functional
+const uploadImg = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    chooseImage({
+      count: 1,
+      fail: res => reject(res)
+    }).then(r => {
+      console.log('选择图片=>', r.tempFilePaths[0])
+      const fileManager = getFileSystemManager();
+      fileManager.readFile({
+        filePath: r.tempFilePaths[0],
+        success: (res) => {
+          if (res && res.data) {
+            console.log('fileManager.readFile', res.data);
+            resolve({ path: r.tempFilePaths[0], data: res.data },)
+          }
+        }
+      })
+    });
+  })
+}
 
-var initials = _.compose(_.join('. ') ,_.map(_.compose(_.toUpperCase, _.head)), _.split(' '));
-initials("hunter stockton thompson");
+
